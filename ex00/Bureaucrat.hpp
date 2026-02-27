c4c8b47f95b5f9eb3b7341ac14990966aa5a250a
Tomasz Szymanski <tomasz_szymanski@outlook.com>
2026-02-27 19:56:53 +0100

    ex00 done

---
+++ ex00/Bureaucrat.hpp
@@ -0,0 +1,47 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+#include <string>
+#include <exception>
+
+class Bureaucrat {
+	public:
+		// Orthodox Canonical Form
+		Bureaucrat();									// Default constructor
+		Bureaucrat(const std::string name, int grade);	// Parametrized constructor
+		Bureaucrat(const Bureaucrat& src);				// Copy constructor
+		Bureaucrat& operator=(const Bureaucrat& rhs);	// Copy assignment operator
+		~Bureaucrat();									// Destructor
+		
+		// Getters
+		std::string	getName() const;
+		int			getGrade() const;
+
+		// Methods
+		void	incrementGrade();
+		void	decrementGrade();
+	
+		// Excepstions
+		class GradeTooHighException : public std::exception {
+			public:
+			virtual const char* what() const throw() {
+				return "Grade is too high! (Max Grade = 1)";
+			}
+		};
+		class GradeTooLowException : public std::exception {
+			public:
+			virtual const char * what() const throw() {
+				return "Grade is too low! (Min Grade = 150)";
+			}
+		};
+
+	private:
+	const std::string 	_name; // Constant name
+	int					_grade; // Range 1 to 150
+};
+
+// Instertion operator overload
+std::ostream& operator<<(std::ostream& o, const Bureaucrat& b);
+
+#endif
---
+++ ex00/main.cpp
@@ -0,0 +1,50 @@
+#include "Bureaucrat.hpp"
+
+int main() {
+	// Test 1: Valid construction and printing
+	// Since exceptions are used: try - catch should be used
+	try {
+		std::cout << "// Test 1: Valid construction and printing" << std::endl;
+		Bureaucrat b1("John", 2);
+		std::cout << b1;
+		b1.incrementGrade();
+		std::cout << "After increment: " << b1;
+	} catch (std::exception &e) {
+		std::cerr << e.what() << std::endl;
+	}
+
+	//Test 2: Grade too high during construction
+	try {
+		std::cout << "//Test 2: Grade too high during construction" << std::endl;
+		Bureaucrat b2("Tom", 0);
+	} catch (std::exception &e) {
+		std::cerr << "Caught expected error: " << e.what() << std::endl;
+	}
+
+	//Test 2: Grade too high during incrementation
+	try {
+		std::cout << "//Test 2: Grade too high during incrementation" << std::endl;
+		Bureaucrat b3("Paul", 2);
+		std::cout << b3;
+		b3.incrementGrade();
+		std::cout << "After increment: " << b3;
+		b3.incrementGrade();
+		std::cout << "After increment: " << b3;
+	} catch (std::exception &e) {
+		std::cerr << e.what() << std::endl;
+	}
+
+	//Test 2: Grade too low during incrementation
+	try {
+		std::cout << "//Test 2: Grade too low during incrementation" << std::endl;
+		Bureaucrat b4("Adam", 148);
+		b4.decrementGrade();
+		std::cout << "After decrement: " << b4;
+		b4.decrementGrade();
+		std::cout << "After decrement: " << b4;
+		b4.decrementGrade();
+		std::cout << "After decrement: " << b4;
+	} catch (std::exception &e) {
+		std::cerr << e.what() << std::endl;
+	}
+}
